08e3085fc581a9f5d870241ffbcb27590d16507c
Nahidul Islam <contact.nahidul@gmail.com>
2024-03-01 10:11:55 +0600

    laravel database queries

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        // DB::listen(function ($query) {
+        //     var_dump($query->sql);
+        //     var_dump($query->binding ?? 'no binding');
+        //     var_dump($query->time);
+        // });
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,62 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+
+    // $pdo = DB::connection()->getPdo();
+
+    // $users = $pdo->query('select * from users')->fetchAll();
+
+    $result = DB::select('select * from users where id =? and name = ?', [1, 'vilma o\'Keefe']);
+
+    // $result = DB::select('select * from users where id = :id', ['id' => 1]);
+
+    // DB::insert('insert into users (name, email, password) values (?, ?, ?)', ['Dayle', 'mail@app.com', 'password']);
+
+    // DB::update('update users set email = "test@app.com" where email = ?', ['mail@app.com']);
+
+    // DB::delete('delete from users where id = ?', [4]);
+
+    // DB::statement('truncate table users');
+
+    // DB::select('select * from users');
+
+    // DB::table('users')->select()->get();
+
+    // User::all();
+
+
+    DB::transaction(function () {
+
+        try {
+            DB::table('users')->delete();
+
+            $result = DB::table('users')->where('id', 4)->update(['email' => 'none']);
+
+            if (!$result) {
+                throw new \Exception;
+            }
+        } catch (\Exception $e) {
+
+            DB::rollBack();
+        }
+    }, 5); //optional argument how many times, transaction should be reattemted.
+
+
+
+    return view('welcome');
+});
